9431d10d4429b03f9b4568ab522fb36136c47420
Lance Moore <lmoore@engineerroi.com>
2021-02-06 23:53:34 +0200

    added favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,8 +38,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    '@nuxtjs/robots'
+    '@nuxtjs/axios'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
