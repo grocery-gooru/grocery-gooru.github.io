4af1277c0022b03cb8ead2d4595a8484dfd940df
Lance Moore <lmoore@engineerroi.com>
2021-02-11 07:57:35 +0200

    Added CNAME file and nuxt.config changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,12 +3,3 @@
     <Nuxt />
   </div>
 </template>
-<script>
-export default {
-  head() {
-    return {
-      title: process.env.VUE_APP_NAME,
-    }
-  }
-}
-</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-guru',
+    title: 'Grocery Guru',
     htmlAttrs: {
       lang: 'en'
     },
